d904168828397a88addc08b0cd6e915599e5ff24
Jordan Verasamy <jordan.verasamy@shopify.com>
2022-03-16 12:28:41 -0400

    copy custom zsh theme to right place in install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ~/dotfiles/alephnaut-gnzh.zsh-theme ~/.oh-my-zsh/custom/themes/alephnaut-gnzh.zsh-theme
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
