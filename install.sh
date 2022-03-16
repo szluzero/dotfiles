0c387b117a657df663649774db9c3dd442976c3e
Jordan Verasamy <jordan.verasamy@shopify.com>
2022-03-16 13:04:09 -0400

    fix clone location... again

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 
 # copy custom theme to correct directory
 cp ~/dotfiles/alephnaut-gnzh.zsh-theme ~/.oh-my-zsh/custom/themes/alephnaut-gnzh.zsh-theme
